b28e36b7ad2d558daa7ce29d7b426153b1172556
prohause@abv.bg <Sevdjan8092>
2019-03-17 07:06:41 +0200

    P01 solved

--- AdvancedQuerying/BookShop/StartUp.cs
+++ AdvancedQuerying/BookShop/StartUp.cs
@@ -1,7 +1,10 @@
-﻿namespace BookShop
+﻿using BookShop.Models.Enums;
+using System;
+using System.Linq;
+
+namespace BookShop
 {
     using Data;
-    using Initializer;
 
     public class StartUp
     {
@@ -9,8 +12,17 @@
         {
             using (var db = new BookShopContext())
             {
-                DbInitializer.ResetDatabase(db);
+                //DbInitializer.ResetDatabase(db);
+                var result = GetBooksByAgeRestriction(db, "miNor");
+                Console.WriteLine(result);
             }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command.ToLower(), true);
+            var books = context.Books.Where(b => b.AgeRestriction == ageRestriction).Select(b => b.Title).OrderBy(x => x).ToList();
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
