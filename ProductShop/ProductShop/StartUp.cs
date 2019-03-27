1fbc11922da2d1c96f2444f72888d2324fd7e0f7
prohause@abv.bg <Sevdjan8092>
2019-03-27 13:42:47 +0200

    P04 solved

--- ProductShop/ProductShop/StartUp.cs
+++ ProductShop/ProductShop/StartUp.cs
@@ -17,23 +17,46 @@ namespace ProductShop
                 //context.Database.EnsureDeleted();
                 //context.Database.EnsureCreated();
 
-                var input = File.ReadAllText(@"C:\Users\proha\source\repos\AdvancedDB\ProductShop\ProductShop\Datasets\products.json");
+                var input = File.ReadAllText(@"C:\Users\proha\source\repos\AdvancedDB\ProductShop\ProductShop\Datasets\categories-products.json");
 
-                var result = ImportProducts(context, input);
+                var result = ImportCategoryProducts(context, input);
                 Console.WriteLine(result);
             }
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoriesProducts = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+
+            context.CategoryProducts.AddRange(categoriesProducts);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<List<Category>>(inputJson)
+                .Where(c => c.Name != null && c.Name.Length >= 3 && c.Name.Length <= 15).ToList();
+
+            context.Categories.AddRange(categories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
         }
 
         public static string ImportProducts(ProductShopContext context, string inputJson)
         {
             var products = JsonConvert.DeserializeObject<List<Product>>(inputJson)
-                .Where(p => p.Name != null && p.Name.Length >= 3 && p.Name.Length <= 15).ToList();
+                .Where(p => p.Name != null && p.Name.Length >= 3).ToList();
 
             context.Products.AddRange(products);
 
-            var countOfAddedProducts = context.SaveChanges();
+            context.SaveChanges();
 
-            return $"Successfully imported {countOfAddedProducts}";
+            return $"Successfully imported {products.Count}";
         }
 
         public static string ImportUsers(ProductShopContext context, string inputJson)
