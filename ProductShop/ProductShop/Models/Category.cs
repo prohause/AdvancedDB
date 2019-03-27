72770fdd6fdb98171c48fc13996e6034677f320b
prohause@abv.bg <Sevdjan8092>
2019-03-27 11:08:05 +0200

    Commit 13:0727.03.2019

---
+++ ProductShop/ProductShop/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        public Category()
+        {
+            CategoryProducts = new List<CategoryProduct>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public ICollection<CategoryProduct> CategoryProducts { get; set; }
+    }
+}
---
+++ ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using ProductShop.Data;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new ProductShopContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            return "";
+        }
+    }
+}
